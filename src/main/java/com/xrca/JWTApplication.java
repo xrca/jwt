92b5ecc4fb113b3d642eb55a43d0c6cc5586b02c
xrca <fishsaury@163.com>
2020-12-06 14:14:25 +0800

    init project

---
+++ src/main/java/com/xrca/JWTApplication.java
@@ -0,0 +1,16 @@
+package com.xrca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xrca
+ * @description jwt
+ * @date 2020/11/6 22:31
+ **/
+@SpringBootApplication
+public class JWTApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JWTApplication.class, args);
+    }
+}
